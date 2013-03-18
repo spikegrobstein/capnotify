2dedf8f01102da7e56e327d3937250cd769ee575
Spike Grobstein <me@spike.cx>
2013-03-18 08:24:52 -0400

    beginnings of some working tests

--- spec/capnotify_spec.rb
+++ spec/capnotify_spec.rb
@@ -8,13 +8,6 @@ describe Capnotify do
 
   before do
     Capnotify.load_into(config)
-
-    # default capistrano config
-    config.load do
-      set :application, 'testapp'
-    end
-
-    RestClient.stub(:post)
   end
 
   let(:capnotify) { config.capnotify }
@@ -37,6 +30,74 @@ describe Capnotify do
       it "should trigger :maintenance_page_up before deploy:web:disable"
 
       it "should trigger :maintenance_page_down after deploy:web:enable"
+    end
+
+  end
+
+  context "built-in messages" do
+
+    context "when application is not specified" do
+
+      before do
+        config.load do
+          set :stage, 'production'
+        end
+      end
+
+      it "should not raise an error" do
+        lambda { config.capnotify_appname }.should_not raise_error
+      end
+
+    end
+
+    context "when stage is not specified" do
+
+      before do
+        config.load do
+          set :application, 'MyApp'
+        end
+      end
+
+      it "should not raise an error" do
+        lambda { config.capnotify_appname }.should_not raise_error
+      end
+
+    end
+
+    context "when the user wants to override capnotify_appname" do
+      before do
+        config.load do
+          set :capnotify_appname, 'SimpleApp'
+        end
+      end
+
+      it "should return what the user overrode with" do
+        config.capnotify_appname.should == 'SimpleApp'
+      end
+
+    end
+
+    context "when both application and stage are specified" do
+
+      before do
+        config.load do
+          set :application, 'SimpleApp'
+          set :stage, 'production'
+        end
+      end
+
+      it "should not raise an error" do
+        lambda { config.capnotify_appname }.should_not raise_error
+      end
+
+      it "should include the application name" do
+        config.capnotify_appname.should match('SimpleApp')
+      end
+
+      it "should include the stage name" do
+        config.capnotify_appname.should match('production')
+      end
+
     end
 
   end
