2a71ea566ffb64cb86a7e08e8a6e9153ddec5e04
Spike Grobstein <me@spike.cx>
2013-03-19 08:37:12 -0400

    don't blow up if :scm is not defined

--- lib/capnotify.rb
+++ lib/capnotify.rb
@@ -18,7 +18,7 @@ module Capnotify
       _cset :capnotify_deployment_notification_text_template_path, capnotify.built_in_template_for('default_notification.txt.erb')
 
       _cset(:deployer_username) do
-        if fetch(:scm, '').to_sym == :git
+        if exists?(:scm) && fetch(:scm).to_sym == :git
           `git config user.name`.chomp
         else
           `whoami`.chomp
