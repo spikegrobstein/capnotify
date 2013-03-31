1d0a541b054d3bbf173d2fcf15ead28438cbb5ea
Spike Grobstein <me@spike.cx>
2013-03-31 09:52:17 -0400

    updated some comments in Component

--- lib/capnotify/component.rb
+++ lib/capnotify/component.rb
@@ -4,9 +4,12 @@ module Capnotify
     # content can only be of these types of classes
     VALID_CONTENT_CLASSES = [ String, Hash, Array ]
 
-    attr_accessor :header, :name, :css_class
-    attr_reader :content
+    attr_accessor :header, :name
 
+    # the class(s) for this component (as a string)
+    attr_accessor :css_class#, :custom_css
+
+    # a block that will configure this instance lazily
     attr_reader :builder
 
     def initialize(name, options={}, &block)
@@ -20,14 +23,24 @@ module Capnotify
       end
     end
 
+    # assign the content as new_content
     def content=(new_content)
       raise ArgumentError, "content must be a #{ VALID_CONTENT_CLASSES.join(', ') }" unless VALID_CONTENT_CLASSES.include?(new_content.class)
 
       @content = new_content
     end
 
+    def content
+      @content
+    end
+
+    # call @builder with self as a param if @builder is present
+    # ensure builder is nil
+    # then return self
     def build!
-      @builder.call(self) and @builder = nil unless @builder.nil?
+      @builder.call(self) unless @builder.nil?
+
+      @builder = nil
 
       return self
     end
