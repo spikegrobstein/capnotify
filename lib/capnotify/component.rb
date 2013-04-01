57627056aa5bcbb3c2c40fb80e8835a2fe169d5f
Spike Grobstein <me@spike.cx>
2013-04-01 18:59:33 -0400

    added comments for the new component functions

--- lib/capnotify/component.rb
+++ lib/capnotify/component.rb
@@ -56,21 +56,29 @@ module Capnotify
       ERB.new( File.open( template_path_for(format) ).read, nil, '%<>' ).result(self.get_binding)
     end
 
+    # return the binding for this object
+    # this is needed when embedding ERB templates in each other
     def get_binding
       binding
     end
 
+    # set the template path for this particular instance
+    # the template path is the path to the parent directory of a renderer ERB template
     def template_path_for(format)
       @renderers ||= {}
 
       File.join( @template_path || @@default_template_path, @renderers[format] || @@default_renderers[format])
     end
 
+    # create renderers
+    # given a key for the format, provide the name of the ERB template to use to render relative to the template path
     def render_for(renderers={})
       @renderers ||= {}
       @renderers = @renderers.merge(renderers)
     end
 
+    # set the template_path for this instance
+    # the template_path is the parent directory of the ERB template used to render the component's content
     def template_path(new_template_path)
       @template_path = new_template_path
     end
